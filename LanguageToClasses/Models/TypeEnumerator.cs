eca5df56037fa5fe128e0767cf86e9b3a96745d8
Leo <samba25leo@gmail.com>
2019-07-09 19:18:46 -0300

    redo

---
+++ LanguageToClasses/Models/TypeEnumerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LanguageToClasses.Models
+{
+	[Flags]
+	public enum IntegralTypeEnumerator
+	{
+		sbyteType = 1,
+		ushortType = 2,
+		shortType = 4,
+		uintType = 8,
+		intType = 16,
+		ulongType = 32,
+		longType = 64,
+		byteType = 128
+	}
+
+	[Flags]
+	public enum FloatingTypeEnumerator
+	{
+		doubleType = 1,
+		decimalType = 2,
+		floatType = 4
+	}
+
+	[Flags]
+	public enum SpecialTypeEnumerator
+	{
+		datetime = 1,
+		boolean = 2,
+		charType = 4
+	}
+}
