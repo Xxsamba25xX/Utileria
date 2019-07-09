480c60f6dfee40dd9429292d6076e8814b0a3a5a
Leo <samba25leo@gmail.com>
2019-07-09 19:16:58 -0300

    Matrix Generator

---
+++ Utileria/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utileria.Extensions
+{
+    public static class EnumerableExtensions
+    {
+
+        public static void Shuffle<T>(this IList<T> me)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < me.Count; i++)
+            {
+                var auxiliar = me[i];
+                var newPos = rnd.Next(i, me.Count);
+                me[i] = me[newPos];
+                me[newPos] = auxiliar;
+            }
+        }
+
+        public static string GetString(this Array me)
+        {
+            StringBuilder sb = new StringBuilder();
+            var max = me.LongLength;
+            var lineCount = (int)Math.Pow(max, 1d / me.Rank);
+            for (int i = 0; i < max; i++)
+            {
+                var x = i % lineCount;
+                var y = i / lineCount;
+
+                if (x == 0 && i != 0)
+                    sb.AppendLine();
+
+                sb.Append(me.GetValue(x, y) + " ");
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Utileria/ObjectUtils/CharUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utileria.ObjectUtils
+{
+    public static class CharUtils
+    {
+        public static IEnumerable<char> GetRange(int minIndex, int maxIndex = -1)
+        {
+            if (maxIndex < 0) maxIndex = 256;
+            if (maxIndex < minIndex) throw new ArgumentException("minIndex es mayor que el máximo, que te está pasando?");
+
+            List<char> chars = new List<char>();
+            for (int i = minIndex; i < maxIndex; i++)
+            {
+                chars.Add((char)i);
+            }
+
+            return chars;
+        }
+    }
+}
